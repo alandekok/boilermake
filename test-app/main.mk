1b3ab5db5e8bbb4444e24a673eef01a8129c095f
Alan T. DeKok <aland@freeradius.org>
2010-02-09 23:24:01 +0100

    Added rule to install MAN pages
    Along with sample "man" page.

--- test-app/main.mk
+++ test-app/main.mk
@@ -1,9 +1,3 @@
 CXXFLAGS := -g -O0 -Wall -pipe
 
 SUBMAKEFILES := talk.mk animals/animals.mk plants/plants.mk
-
-# Common definitions for installation
-INSTALL	 := $(shell which install)
-prefix   := /usr/local
-bindir   := ${prefix}/bin
-libdir   := ${prefix}/lib
