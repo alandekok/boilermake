afe5e4319f0e76611de3b0ae9c84577c203cc866
Alan T. DeKok <aland@freeradius.org>
2010-05-13 10:25:40 +0200

    Added copyright and license

--- install.mk
+++ install.mk
@@ -1,3 +1,20 @@
+# boilermake: A reusable, but flexible, boilerplate Makefile.
+#
+# Copyright 2008, 2009, 2010 Dan Moulding, Alan T. DeKok
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # INSTALL_NAME - Function to return the name of the file which should
 #   be installed.  For non-libtool builds, it's just the name of the
 #   file (executable or library).
