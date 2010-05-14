6ac1993b68470c2b16ef4b082302613e5bcbc431
Alan T. DeKok <aland@freeradius.org>
2010-05-14 19:13:25 +0200

    We don't need BUILD_DIR or TARGET_DIR any more for libanimals.a
    They just Do The Right Thing.

--- test-app/animals/main.mk
+++ test-app/animals/main.mk
@@ -6,9 +6,6 @@
 # libanimals.a library by itself by running "make" from within the "animals"
 # subdirectory.
 
-BUILD_DIR  := ../build/animals
-TARGET_DIR := ..
-
 CXXFLAGS := -g -O0 -Wall -pipe
 
 SUBMAKEFILES := animals.mk
